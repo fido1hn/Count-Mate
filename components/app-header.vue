a96ae6b0541ae3cf848205f0d32c35a3b13305af
Confidence A.E. <confidoekeanya@gmail.com>
2024-06-01 03:19:46 +0100

    summary section and period picker

---
+++ components/app-header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="flex items-center justify-between mt-5">
+    <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar" />
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { transactionViewOptions } from '~/constants';
+const selectedView = ref(transactionViewOptions[1]);
+</script>
+
+<style></style>
