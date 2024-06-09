5fccdd34fdb7c5d3cef167b698594b70a8a5dd36
Confidence A.E. <confidoekeanya@gmail.com>
2024-06-09 20:28:52 +0100

    user profile storing and storing user view preference

--- pages/index.vue
+++ pages/index.vue
@@ -71,7 +71,9 @@
 <script lang="js" setup>
 import { transactionViewOptions } from '~/constants'
 
-const selectedView = ref(transactionViewOptions[1])
+const user = useSupabaseUser()
+
+const selectedView = ref(user.value.user_metadata?.transaction_view ?? transactionViewOptions[1])
 const isOpen = ref(false)
 const {current, previous} = useSelectedTimePeriod(selectedView)
 
