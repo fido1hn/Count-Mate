b3901f31244e55750335a3073e830630456fd80a
Confidence A.E. <confidoekeanya@gmail.com>
2024-06-10 01:08:38 +0100

    uploading avatar to supabase storage

--- pages/settings/avatar.vue
+++ pages/settings/avatar.vue
@@ -1,7 +1,93 @@
 <template>
-  <div> Avatar </div>
+  <div>
+    <div class="mb-4">
+      <UFormGroup
+        label="Current avatar"
+        class="w-full"
+        help="This would be blank by default">
+        <UAvatar
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          size="3xl" />
+      </UFormGroup>
+    </div>
+
+    <div class="mb-4">
+      <UFormGroup
+        label="New avatar"
+        class="w-full"
+        name="avatar"
+        help="After choosing an image click Save to actually upload the new avatar">
+        <UInput type="file" ref="fileInput" />
+      </UFormGroup>
+    </div>
+
+    <UButton
+      type="submit"
+      color="black"
+      variant="solid"
+      label="Save"
+      :loading="uploading"
+      :disabled="uploading"
+      @click="saveAvatar" />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
 
-<style></style>
+// We need to get the actual avatar URL
+const { toastSuccess, toastError } = useAppToast();
+
+const uploading = ref(false);
+const fileInput = ref(); // Reference to an input with ref="fileInput" attribute
+
+console.log(user.value);
+
+const saveAvatar = async () => {
+  // 1. Get the uploaded file
+  const file = fileInput.value.input.files[0];
+
+  //    a) If no file uploaded, show toast error
+  if (!file) return toastError({ title: 'Select a file to upload first' });
+
+  // 2. Generate the new filename
+  const fileExt = file.name.split('.')[1];
+  const fileName = `${Math.random()}.${fileExt}`;
+
+  try {
+    uploading.value = true;
+
+    // 1. Grab the current avatar URL
+    const currentAvatarUrl = user.value?.user_metadata.avatar_url;
+
+    // 2. Upload the image to avatars bucket
+    const { error } = await supabase.storage
+      .from('avatars')
+      .upload(fileName, file);
+
+    if (error) throw error;
+
+    // 3. Update the user metadata with the avatar URL
+    await supabase.auth.updateUser({
+      data: {
+        avatar_url: fileName,
+      },
+    });
+    // 4. (OPTIONALLY) remove the old avatar file
+    // 5. Reset the file input
+    fileInput.value.input.value = null;
+
+    toastSuccess({
+      title: 'Avatar uploaded',
+    });
+  } catch (error) {
+    toastError({
+      title: 'Error uploading avatar',
+      description: error.message,
+    });
+  } finally {
+    uploading.value = false;
+  }
+};
+</script>
