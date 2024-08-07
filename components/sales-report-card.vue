650d8d030fca140664226de11169ffa8c746e7a0
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-07 02:54:43 +0100

    desktop: initial dashboard complete iteration

--- components/sales-report-card.vue
+++ components/sales-report-card.vue
@@ -20,9 +20,90 @@
       <UButton label="30 days" color="gray" variant="ghost" />
       <UButton label="7 days" color="gray" variant="ghost" />
     </UButtonGroup>
+
+    <BarChart
+      index="name"
+      :data="data"
+      :categories="['total', 'predicted']"
+      :y-formatter="
+        (tick, i) => {
+          return typeof tick === 'number'
+            ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+            : '';
+        }
+      "
+      :rounded-corners="7"
+      :type="'stacked'"
+      :colors="['#845ED7', '#333741']"
+    />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { BarChart } from "@/components/ui/chart-bar";
+
+const data = [
+  {
+    name: "Jan",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Feb",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Mar",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Apr",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "May",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Jun",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Jul",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Aug",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Sep",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Oct",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Nov",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: "Dec",
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+];
+</script>
 
 <style></style>
