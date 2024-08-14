40ce186bc72acebb10e668abc2567c06049b6492
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-14 05:06:32 +0100

    homepage, featured-in section

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,21 @@
         </div>
       </section>
     </div>
+    <section class="p-28">
+      <div class="flex flex-col justify-center">
+        <h2
+          class="mb-9 text-center text-base font-black uppercase text-gray-600"
+        >
+          As featured in
+        </h2>
+        <div class="flex items-center justify-around">
+          <img src="/business-insider.svg" alt="Company Logo" />
+          <img src="/wired.svg" alt="Company Logo" />
+          <img src="/techcrunch.svg" alt="Company Logo" />
+          <img src="/the-guardian.svg" alt="Company Logo" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
