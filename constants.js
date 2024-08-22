3fc0ef1ae25d5a2a426f5d099dffbd494cf8e08f
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-22 21:41:05 +0100

    update: version updates

--- constants.js
+++
@@ -1,3 +0,0 @@
-export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily'];
-export const categories = ['Food', 'Car', 'Housing', 'Entertainment', 'Other'];
-export const transactionTypes = ['Income', 'Expense', 'Saving', 'Investment'];
