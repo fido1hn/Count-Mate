64b1fd3b74129e0e860763ffe5975a9735975d43
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-24 04:39:18 +0100

    return reactive values from userDetails composable

--- composables/userDetails.ts
+++ composables/userDetails.ts
@@ -1,8 +1,9 @@
 const user = useSupabaseUser();
 
 export const useUserDetails = () => {
-  const userFullName = user.value?.user_metadata.full_name;
-  const userEmailAddress = user.value?.email;
+  const userFullName = ref(user.value?.user_metadata.full_name);
+  const userEmailAddress = ref(user.value?.email);
+
   return {
     userFullName,
     userEmailAddress,
