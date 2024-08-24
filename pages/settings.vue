ac2366a3e2a082bf68971344310136ec24b83881
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-24 03:46:56 +0100

    settings page header

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,6 +1,53 @@
 <template>
   <div>
     <div class="gradient-background h-40 w-full md:h-60"></div>
+    <div
+      class="border-1 -mt-16 flex flex-col gap-3 border-green-500 p-5 md:-mt-12 md:flex-row md:items-center md:justify-between"
+    >
+      <div class="flex flex-col gap-2 md:flex-row md:items-center md:gap-4">
+        <UAvatar
+          src="/evan.jpg"
+          alt="Avatar"
+          size="3xl"
+          class="ring ring-white"
+          :ui="{ size: { '3xl': 'w-24 h-24 md:w-40 md:h-40' } }"
+        >
+          <UIcon
+            name="i-fluent-checkmark-starburst-16-filled"
+            class="absolute bottom-1 right-1 text-blue-500"
+          />
+        </UAvatar>
+        <div>
+          <h2
+            class="text-2xl font-semibold text-black dark:text-white md:text-3xl"
+          >
+            Evan You
+          </h2>
+          <p class="text-xs font-normal text-gray-500 md:text-base">
+            testuser@financetracker.io
+          </p>
+        </div>
+      </div>
+      <div class="flex items-center gap-2">
+        <UButton
+          label="Share"
+          size="sm"
+          variant="outline"
+          color="white"
+          icon="i-tabler-user-plus"
+          :ui="{
+            rounded: 'rounded-lg',
+            variant: { outline: 'ring-gray-300 dark:ring-gray-700' },
+          }"
+        />
+        <UButton
+          label="View Profile"
+          size="sm"
+          variant="solid"
+          :ui="{ rounded: 'rounded-lg' }"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
